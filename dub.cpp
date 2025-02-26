c0244cb29b101f77fae16c9509f027df87bec200
haruo <takeuchigabriel@usp.br>
2025-02-26 17:15:25 -0300

    Initial commit

---
+++ dub.cpp
@@ -0,0 +1,75 @@
+#include "daisy_seed.h"
+#include "daisysp.h"
+
+using namespace daisy;
+using namespace daisysp;
+
+
+DaisySeed hw;
+
+float  volumeValue;
+float  DecayValue;
+float  DepthValue;
+float  TuneValue;
+float  SweepValue;
+float  RateValue;
+bool   lfoStates[4] = {false, false, false, false};
+// Switch lfoButtons[4];
+
+
+enum AdcChannel
+{
+    VolumeKnob,
+    DecayKnob,
+    DepthKnob,
+    TuneKnob,
+    SweepKnob,
+    RateKnob,
+    NUM_ADC_CHANNELS
+};
+
+void init_knobs()
+{
+    AdcChannelConfig my_adc_config[NUM_ADC_CHANNELS];
+    my_adc_config[VolumeKnob].InitSingle(daisy::seed::A0);
+    my_adc_config[DecayKnob].InitSingle(daisy::seed::A1);
+    my_adc_config[DepthKnob].InitSingle(daisy::seed::A2);
+    my_adc_config[TuneKnob].InitSingle(daisy::seed::A3);
+    my_adc_config[SweepKnob].InitSingle(daisy::seed::A4);
+    my_adc_config[RateKnob].InitSingle(daisy::seed::A5);
+}
+
+
+void AudioCallback(AudioHandle::InputBuffer  in,
+                   AudioHandle::OutputBuffer out,
+                   size_t                    size)
+{
+    for(size_t i = 0; i < size; i++)
+    {
+        out[0][i] = in[0][i];
+        out[1][i] = in[1][i];
+    }
+}
+
+int main(void)
+{
+    hw.Init();
+    hw.adc.Start();
+    init_knobs();
+    hw.SetAudioBlockSize(4); // number of samples handled per callback
+    hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
+    hw.StartLog();
+    hw.StartAudio(AudioCallback);
+    while(1)
+    {
+        hw.PrintLine("Volume: " FLT_FMT3,
+                     FLT_VAR3(hw.adc.GetFloat(VolumeKnob)));
+        hw.PrintLine("Decay: " FLT_FMT3, FLT_VAR3(hw.adc.GetFloat(DecayKnob)));
+        hw.PrintLine("Depth: " FLT_FMT3, FLT_VAR3(hw.adc.GetFloat(DepthKnob)));
+        hw.PrintLine("Tune: " FLT_FMT3, FLT_VAR3(hw.adc.GetFloat(TuneKnob)));
+        hw.PrintLine("Sweep: " FLT_FMT3, FLT_VAR3(hw.adc.GetFloat(SweepKnob)));
+        hw.PrintLine("Rate: " FLT_FMT3, FLT_VAR3(hw.adc.GetFloat(RateKnob)));
+        hw.PrintLine("\n");
+        System::Delay(1000);
+    }
+}
