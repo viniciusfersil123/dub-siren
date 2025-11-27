36f63668fbb81ec6bd267160846a87d5edda44b0
haruo <takeuchigabriel@usp.br>
2025-11-27 02:07:40 -0300

    inverte a leitura dos knobs (o hardware esta invertido)

--- dub.cpp
+++ dub.cpp
@@ -70,26 +70,26 @@ void KnobHandlerDaisy::InitAll()
 void KnobHandlerDaisy::UpdateAll()
 {
     // VCO tune knobs
-    vco->TuneValue = fclamp(hw.adc.GetFloat(TuneKnob), 0.f, 1.f);
+    vco->TuneValue = 1.f - fclamp(hw.adc.GetFloat(TuneKnob), 0.f, 1.f);
 
     // Decay Envelope knobs
-    envelope->ReleaseValue = fclamp(hw.adc.GetFloat(DecayKnob), 0.f, 1.f);
+    envelope->ReleaseValue = 1.f - fclamp(hw.adc.GetFloat(DecayKnob), 0.f, 1.f);
 
     // Only update sweep value while a trigger is pressed
     if(triggers->Pressed())
     {
         sweep->ReleaseValue
-            = fmap(hw.adc.GetFloat(SweepKnob), 0.f, 1.f, Mapping::LINEAR);
+            = fmap(1.f - hw.adc.GetFloat(SweepKnob), 0.f, 1.f, Mapping::LINEAR);
     }
 
     // LFO depth (vibrato intensity 0-100%) and rate knobs
-    lfo->DepthValue = fclamp(hw.adc.GetFloat(DepthKnob), 0.f, 1.f);
+    lfo->DepthValue = 1.f - fclamp(hw.adc.GetFloat(DepthKnob), 0.f, 1.f);
     lfo->RateValue  = fmap(
-        hw.adc.GetFloat(RateKnob), LFO_MIN_FREQ, LFO_MAX_FREQ, Mapping::EXP);
+        1.f - hw.adc.GetFloat(RateKnob), LFO_MIN_FREQ, LFO_MAX_FREQ, Mapping::EXP);
 
     // OutAmp volume knob
     out_amp->VolumeValue
-        = fmap(hw.adc.GetFloat(VolumeKnob), 0.f, 1.f, Mapping::EXP);
+        = fmap(1.f - hw.adc.GetFloat(VolumeKnob), 0.f, 1.f, Mapping::EXP);
 }
 // KnobHandler functions
 
